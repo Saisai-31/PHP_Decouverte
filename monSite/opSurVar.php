e2a84f5da83151551699217d42492a8eac49d217
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-10-16 16:43:24 +0200

    Suite

--- monSite/opSurVar.php
+++ monSite/opSurVar.php
@@ -23,6 +23,20 @@
         $pp = "Copain";
         $affich = $nn. " " .$mm. ", tu veux être mon ami " .$pp. " ?";
         echo $affich;
+        echo "<br>";
+        $a =1;
+        $b= 3;
+        $a = $a + 2;
+        echo " premier : " .$a;
+        echo "<br>";
+        $a = $a * $b;
+        echo "deuxième : " .$a;
+        $a= $a - 1;
+        echo "<br>";
+        echo " troisième : " .$a;
+        echo "<br>";
+        $a = $a / 4;
+        echo "quatrième : " .$a;
 
     ?>
     
