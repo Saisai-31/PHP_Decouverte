0bb1ea423e4b0456e7b6e2ece5d0cf91e0397635
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-10-18 16:36:02 +0200

    suite du cours PHP avec les boucles

--- monSite/boucle.php
+++ monSite/boucle.php
@@ -26,7 +26,9 @@
         <?php
         //La boucle while
         //exécute un bloc de ligne de code tant qu'une condition est vrai.
-
+        echo "Boucle while et do while";
+        echo "<br>";
+        echo "<br>";
         $a= 1;
         //tant que la variable est inférieur à 10
         while($a <= 10){
@@ -64,6 +66,33 @@
         }
         //Tant que la variable est inférieur ou égal à 15
         while($nombre <=15);
+
+        echo "<br><br>.........................................<br><br>";
+
+        /*
+        La boucle 
+        Elle indique de répéter une action un certain nombre de fois en fonction de la valeur du pointeur. La structure possède
+        3 paramètres : un début, un test et une incrémentation qui s'éffectuent à la fin de chaque passage dans la boucle.
+        */
+        echo "Boucle FOR";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        for($p = 0; $p <= 5; $p++){
+            echo "Je dis bonjour avec la boucle for : ".$p."<br>";
+        }
+
+        echo "<br><br>.........................................<br><br>";
+
+        //La boucle Foreach
+        echo "Boucle FOREACH";
+        echo "<br>";
+        $T["Janvier"] = "école";
+        echo "<br>";
+        $T["Septembre"] = "vacance";
+            foreach($T as $mois => $periode){
+                echo $mois. "===> ".$periode. "<br>";
+            }
 
 
 
