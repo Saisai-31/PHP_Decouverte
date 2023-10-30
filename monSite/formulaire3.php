bb6a98d787c0b82d683db41649de7d216d9988f4
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-10-30 16:40:02 +0100

    suite du cours formulaire

---
+++ monSite/formulaire3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleGrid.css">
+    <title>Manipuler les variables</title>
+</head>
+<body>
+
+    <?php
+        include_once('entete.php');
+        include_once('bodyFormulaire3.php');
+        include_once('footer.php');
+    ?>
+
+   
+    
+</body>
+</html>
