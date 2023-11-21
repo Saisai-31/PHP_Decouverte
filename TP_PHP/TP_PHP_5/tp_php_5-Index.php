5babc42d03f571e111e237c0326d20e07bc773d8
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-11-21 11:04:15 +0100

    Suite du TP PHP 5

--- TP_PHP/TP_PHP_5/tp_php_5-Index.php
+++ TP_PHP/TP_PHP_5/tp_php_5-Index.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styleGrid.css">
+    <link rel="stylesheet" href="../../styleGrid.css">
     <title>TP PHP 5</title>
 </head>
 <body>
 
     <?php
-        include_once('../entete.php');
-        include_once('body/tp_php_5-body.php');
-        include_once('../footer.php');
+        include_once('../../entete.php');
+        include_once('tp_php_5-body.php');
+        include_once('../../footer.php');
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- TP_PHP/TP_PHP_5/tp_php_5-body.php
+++ TP_PHP/TP_PHP_5/tp_php_5-body.php
@@ -5,7 +5,7 @@
     <?php
         echo "Exercice 1 : Écrivez un tableau multidimensionnel associatif dont les clés sont des noms de personne et les valeurs des tableaux indicés contenant le prénom, la ville de résidence et l’âge de la personne.<br><br>";
 
-        $personne = array("Zidane" => array("Zinedine", "Madrid", 51), "Pholoppe" => array("Cyril", "Toulouse", 45), "Marley" => array("Bob", "Paradis", 78));
+        $personne = array($Zidane = array("Zinedine", "Madrid", 51), $Pholoppe = array("Cyril", "Toulouse", 45), $Marley = array("Bob", "Paradis", 78));
 
         print_r($personne);
 
@@ -38,7 +38,7 @@
 
 
         echo "Exercice 3 : Utilisez une boucle foreach pour lire les tableaux des exercices 1 et 2. <br><br>";
-        foreach($personne as $cle=>$valeur){
+        foreach($personnes as $cle=>$valeur){
             echo "Clé $cle : ","<br>";
             foreach($valeur as $ind=>$val){
                 echo "Valeur $ind : ", $val, "","<br>";
@@ -56,6 +56,70 @@
 
 
          echo "Exercice 4 : Utilisez une boucle while pour lire les tableaux des exercices 1 et 2.<br><br>";
+         $index = 0;
+         //count($personnes)
+         while($index < 1){
+            print_r($personnes);
+            $index++;
+         }
+         echo "<br><br>";
+
+         echo "Exercice 5 : Créez un tableau contenant une liste d’adresses de sites recommandés, puis créez un lien aléatoire vers le premier site de la liste après avoir trié le tableau en ordre aléatoire.<br>";
+         // shuffle — Mélange les éléments d'un tableau
+         // Suivant le type de tableau et mes besoin, j'aurais pu utiliser : 
+         // array_rand — Prend une ou plusieurs clés, au hasard dans un tableau
+
+        $listeSites = ['www.site1.com', 'www.site2.fr', 'www.site3.org', 'www.site4.net', 'www.site5.th'];
+        print_r($listeSites);
+        echo "<br>";
+        shuffle($listeSites);
+        // Je prends le 1er site de la liste après avoir mélagé le tableau aléatoirement
+        $siteAleatoire = $listeSites[0];
+        // J'affiche le lien vers le site aléatooire
+        echo "Le site recommandé est : ".$siteAleatoire."<br><br>";
+
+
+        echo "Exercice 6 : Créez un tableau d’entiers variant de 1 à 63 puis, à partir de celui-ci, un autre tableau de nombres variant de 0 à 6.3. Créez ensuite un tableau associatif dont les clés X varient de 0 à 6.3 et dont les valeurs sont sin(X). Affichez le tableau de valeurs dans un tableau HTML.<br>";
+        //range — Crée un tableau contenant un intervalle d'éléments. start : Première valeur de la séquence. End : dernière valeur possible de la séquence. Step : indique de combien la séquence produite progresse entre les valeurs de la séquence, step peut être négatif pour les séquences décroissantes. Si step est un float sans partie fractionnaire, il est interprété comme un int.
+        $tableauEntiers = range(1, 63, 1);
+        print_r($tableauEntiers);
+        echo "<br><br>";
+        //array_map — est une fonction en PHP qui applique une fonction à chaque élément d'un tableau et retourne un nouveau tableau avec les résultats de l'application de cette fonction à chaque élément.
+        //function ($num) { return $num / 10; } est une fonction anonyme (ou fonction callback) définie ici à la volée. Elle prend un paramètre $num (chaque élément du tableau $tableauEntiers) et retourne ce nombre divisé par 10.
+        //$tableauEntiers est le tableau d'entiers de 1 à 63, issu de la fonction range(1, 63).
+        $tableauNombres = array_map(function ($num) {
+            return $num / 10;
+        }, $tableauEntiers);
+        print_r( $tableauNombres);
+        echo "<br><br>";
+        //Ce code utilise array_map pour appliquer la fonction anonyme à chaque élément du tableau $tableauEntiers. En conséquence, chaque élément du tableau d'entiers est divisé par 10 et le résultat est stocké dans le tableau $tableauNombres. Cela crée un nouveau tableau où chaque nombre du tableau initial est maintenant divisé par 10.
+
+        //Le sinus d'un nombre est une fonction mathématique qui attribue à chaque nombre réel une valeur comprise entre -1 et 1. Cette fonction est périodique et est souvent représentée sous forme de courbe sinusoïdale.
+        //array_map('sin', $tableauNombres) : Ici, array_map est utilisé pour appliquer la fonction sin à chaque élément du tableau $tableauNombres. La fonction sin est une fonction mathématique standard en PHP qui calcule le sinus d'un nombre. Elle est appliquée à chaque élément du tableau $tableauNombres.
+        //$tableauNombres : Ce tableau contient les nombres de 0 à 6.3 (résultat de la division par 10 des entiers de 1 à 63).
+        //array_combine($tableauNombres, array_map('sin', $tableauNombres)) : La fonction array_combine prend deux tableaux en entrée : le premier tableau $tableauNombres est utilisé comme clés et le deuxième tableau (résultat de l'application de sin à chaque élément de $tableauNombres) est utilisé comme valeurs. Cette fonction combine les deux tableaux pour former un tableau associatif où chaque élément de $tableauNombres est associé à son résultat de la fonction sin.
+        $tableauSinus = array_combine($tableauNombres, array_map('sin', $tableauNombres));
+        //Ainsi, $tableauSinus est un tableau associatif où les clés sont les nombres de 0 à 6.3 et les valeurs sont les résultats de la fonction sinus appliquée à ces nombres.
+
+        //J'affiche  $tableauSinus dans un tableau
+        echo '<table border="1">';
+        echo '<tr><th>X</th><th>sin(X)</th></tr>';
+        foreach ($tableauSinus as $x => $sinX) {
+            echo '<tr>';
+            echo '<td>' . $x . '</td>';
+            echo '<td>' . $sinX . '</td>';
+            echo '</tr>';
+        }   
+        echo '</table>';
+
+
+
+
+
+
+
+
+
         
 
     ?>
