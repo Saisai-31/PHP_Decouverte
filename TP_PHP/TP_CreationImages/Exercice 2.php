93e30636a6bfddb52d163f9de700186b3309b124
Cyril Pholoppe <cyril.pholoppe@gmail.com>
2023-12-11 07:20:12 +0100

    exercice 2 du tp_creaionImages

--- TP_PHP/TP_CreationImages/Exercice 2.php
+++ TP_PHP/TP_CreationImages/Exercice 2.php
@@ -1,10 +1,24 @@
 <?php
-echo "<h2>Créez une image de 400 x 200 pixels avec un fond transparent. Dessinez une suite de rectangles emboîtés de couleurs différentes</h2>.<br><br>";
+    echo "<h2>Créez une image de 400 x 200 pixels avec un fond transparent. Dessinez une suite de rectangles emboîtés de couleurs différentes</h2>.<br><br>";
 
-header('image/png');
-// Création de l'image
-$image = imagecreate(400, 200);
+    header('image/png');
+    // Création de l'image
+    $image2 = imagecreate(400, 200);
 
-$blanc = imagecolorallocate($image, 255, 255, 255, 0.5); // Blanc pour le fond
+    $blanc = imagecolorallocate($image2, 255, 255, 255); // Blanc pour le fond
+    $rouge = imagecolorallocate($image2, 255, 0, 0);
+    $vert = imagecolorallocate($image2, 255, 0, 0);
 
+    imagesetthickness($image2, 3);
+
+    for($i = 3; $i < 100; $i+=20){
+        imagerectangle($image2, $i, $i, 800 - $i, 400 - $i, $rouge);
+    }
+    imagefilledrectangle($image2, 100, 100, 700, 300, $rouge);
+    imagepng($image2, "image2.png");
+
+    imagedestroy($image2);
 ?>
+
+<img src="image2.png" alt="">
+
